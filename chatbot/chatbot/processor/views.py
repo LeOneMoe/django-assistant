da4e8b5f689ad1d4f3ed189c82594deb427d88b9
Олег Взюков < >
2017-03-23 20:38:54 +0300

    added same settings

--- chatbot/chatbot/processor/views.py
+++ chatbot/chatbot/processor/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from . import computing
+
+
+@csrf_exempt
+def compute(request):
+
+    expr = request.POST.get('data', None)
+
+    if expr is None:
+        return JsonResponse({'error': 'No data field specified'})
+
+    is_ok, answer = computing.compute(expr)
+
+    if is_ok:
+        return JsonResponse({'answer': answer})
+    else:
+        return JsonResponse({'error': answer})
--- chatbot/chatbot/settings.py
+++ chatbot/chatbot/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'chatbot.interface',
+    'chatbot.processor'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
